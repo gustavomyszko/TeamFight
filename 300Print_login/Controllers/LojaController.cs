466ad0074b98bdc5ecfa81ff367dbd4b199f7920
André Myszko <andre.myszko@gmail.com>
2020-01-05 21:03:20 -0300

    commit_05/01/2020 - 21:02

---
+++ 300Print_login/Controllers/LojaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using _300Print_login.Models;
+
+namespace _300Print_login.Controllers
+{
+    public class LojaController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ 300Print_login/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _300Print_login.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
